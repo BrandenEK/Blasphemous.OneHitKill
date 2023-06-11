2deab1bf3421689a6424eb66b926ad6c1e24bbdc
Branden Wood <brandenek03912@hotmail.com>
2023-06-11 10:25:54 -0400

    Initial files

---
+++ OneHitKO/DamagePatches.cs
@@ -0,0 +1,15 @@
+﻿using Gameplay.GameControllers.Entities;
+using Gameplay.GameControllers.Penitent.Damage;
+using HarmonyLib;
+
+namespace OneHitKO
+{
+    [HarmonyPatch(typeof(PenitentDamageArea), "TakeDamage")]
+    public class DamageArea_Patch
+    {
+        public static void Prefix(ref Hit hit)
+        {
+            hit.DamageAmount = 1000;
+        }
+    }
+}
---
+++ OneHitKO/OneHitKO.cs
@@ -0,0 +1,9 @@
+﻿using ModdingAPI;
+
+namespace OneHitKO
+{
+    public class OneHitKO : Mod
+    {
+        public OneHitKO(string modId, string modName, string modVersion) : base(modId, modName, modVersion) { }
+    }
+}
