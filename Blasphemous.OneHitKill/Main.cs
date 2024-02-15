8d0b51a48d623eea7806da4c8bf89243bbeaab73
unknown <brandenek03912@hotmail.com>
2024-02-15 18:35:04 -0500

    core: reference v2.1.0

--- Blasphemous.OneHitKill/Main.cs
+++ Blasphemous.OneHitKill/Main.cs
@@ -3,7 +3,7 @@
 namespace Blasphemous.OneHitKill;
 
 [BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
-[BepInDependency("Blasphemous.ModdingAPI", "2.0.1")]
+[BepInDependency("Blasphemous.ModdingAPI", "2.1.0")]
 public class Main : BaseUnityPlugin
 {
     public static OneHitKill OneHitKill { get; private set; }
